cec0fc67d2a271fbffdf933b571b66c71bb89697
Michelle Huntley <michellehuntleys@gmail.com>
2021-08-08 18:30:44 -0700

    updated main.py to have upload file functionality

--- app/main.py
+++ app/main.py
@@ -1,8 +1,34 @@
-from flask import Flask, jsonify, request
-from app.torch_utils import transform_image, get_prediction
+import os
+from werkzeug.utils import secure_filename
+from flask import Flask, jsonify, flash, request, redirect, send_file, render_template
+from torch_utils import transform_image, get_prediction #app.torch_utils
 import traceback
 
-app = Flask(__name__)
+UPLOAD_FOLDER = 'uploads/'
+
+app = Flask(__name__, template_folder='templates')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/uploadfile', methods=['GET', 'POST'])
+def upload_file():
+	if request.method == 'POST':
+		# check if the post request has the file part
+		if 'file' not in request.files:
+			print('no file')
+			return redirect(request.url)
+		file = request.files['file']
+		# if user does not select file, browser submits empty part without filename
+		if file.filename == '':
+			print('no filename')
+			return redirect(request.url)
+		else:
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			print('saved file successfully')
+		# send file name as parameter to download
+			#return redirect('/downloadfile/' + filename)
+			return redirect('/displayresults/', value = filename)
+	return render_template('index.html')
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 def allowed_file(filename):
@@ -28,9 +54,19 @@ def predict():
 			traceback.print_exc()
 			return jsonify({'error': 'error during prediction'})
 
+@app.route('/return-files/<filename>')
+def return_files_tut(filename):
+    file_path = UPLOAD_FOLDER + filename
+    return send_file(file_path, as_attachment=True, attachment_filename='')
+
+@app.route('/')
+def index():
+	#return "Flask app deployed"
+    return render_template('index.html')
+
 @app.route('/hello')
 def hello():
 	return 'Hello world!'
-#
-# if __name__ == '__main__':
-# 	app.run()
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
