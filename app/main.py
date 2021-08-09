6e00c0dae16110428924ba85697af18f5f25942a
Michelle Huntley <michellehuntleys@gmail.com>
2021-08-08 19:19:54 -0700

    updated file upload in main.py

--- app/main.py
+++ app/main.py
@@ -14,13 +14,11 @@ def upload_file():
 	if request.method == 'POST':
 		# check if the post request has the file part
 		if 'file' not in request.files:
-			print('no file')
-			return redirect(request.url)
+			return "No file found."
 		file = request.files['file']
 		# if user does not select file, browser submits empty part without filename
 		if file.filename == '':
-			print('no filename')
-			return redirect(request.url)
+			return "No file name found."
 		else:
 			filename = secure_filename(file.filename)
 			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
